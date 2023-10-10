2b12b61062a800927eb6e70ef951d342795762b7
Matvey Pichaikin <vpicajkin@gmail.com>
2023-10-10 18:22:30 +0500

    third commit

--- 3_file.cpp
+++ 3_file.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
 
-float square(float number); 
+float get_new_number(float, int); 
 
 int main(void)
 {
     float number;
-    cout << endl << "Введите число квадрат которого вы хотите получить: ";
+    cout << endl << "Введите число которое вы хотите возвести в степень: ";
     cin >> number;
-    float new_number = square(number);
-    cout << "Квадрат числа " << number << " = " << new_number <<endl;
+    int degree;
+    cout << "Введите натуральную степень, в которую вы хотите возвести число: ";
+    cin >> degree;
+    float new_number = get_new_number(number, degree);
+    cout << "Число " << number << " в степени " << degree << " = " << new_number <<endl;
     return 0;
 }
-float square(float number)
+
+float get_new_number(float number, int degree)
 {
-    return number * number;
+    int number_1 = number;
+    for (int i = 1; i < degree; i++)
+    {
+        number = number * number_1;
+    }
+    return number;
 }
