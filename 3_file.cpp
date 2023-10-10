ed77820bb6febe1601a8efa9c7ed05628ec1d35f
Matvey Pichaikin <vpicajkin@gmail.com>
2023-10-10 17:12:52 +0500

    first commit

---
+++ 3_file.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    float number;
+    cout << endl << "Введите число квадрат которого вы хотите получить: ";
+    cin >> number;
+    cout << "Квадрат числа " << number <<" = " << number * number <<endl;
+    return 0;
+}
