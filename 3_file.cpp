0ea600ca96c8a86696822a6dc98ff775efc6aa95
Matvey Pichaikin <vpicajkin@gmail.com>
2023-10-10 17:24:05 +0500

    second commit

--- 3_file.cpp
+++ 3_file.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
+float square(float number); 
+
 int main(void)
 {
     float number;
     cout << endl << "Введите число квадрат которого вы хотите получить: ";
     cin >> number;
-    cout << "Квадрат числа " << number <<" = " << number * number <<endl;
+    float new_number = square(number);
+    cout << "Квадрат числа " << number << " = " << new_number <<endl;
     return 0;
+}
+float square(float number)
+{
+    return number * number;
 }
